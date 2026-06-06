a0081085749e77f221ec16bcb4924c120661d03c
Lidio Riul <lidioriul@gmail.com>
2026-06-06 19:27:11 -0300

    Alterando libs e incluindo spring boot

---
+++ microservices/study-microservices-person/src/main/java/study/software/architecture/person/ApplicationPerson.java
@@ -0,0 +1,13 @@
+package study.software.architecture.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationPerson {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationPerson.class, args);
+    }
+
+}
